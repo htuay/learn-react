e7744837131083eb3fe477e6502439f8571932f6
Howard Tuay <htuay@hotmail.com>
2022-06-10 12:25:24 +0800

    Initial commit

---
+++ Learn/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
+
+
+export default function App(){
+    return(
+        <div className="container">
+            <Navbar />
+            <Main />
+        </div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<App />)
